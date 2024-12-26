be3f234709c0965d34b31483688e8dd8e8320f7a
GrigoreBotnarenco <botnarenco1996@mail.com>
2024-12-27 00:48:19 +0200

    section home

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+document.addEventListener('click', function(event) {
+    const burgerCheckbox = document.getElementById('burger-checkbox');
+    const menuList = document.querySelector('.menu-list');
 
+    // Verifică dacă clicul a fost în afara meniului burger
+    if (!menuList.contains(event.target) && !burgerCheckbox.contains(event.target)) {
+        burgerCheckbox.checked = false; // Ascunde meniul
+    }
+});
